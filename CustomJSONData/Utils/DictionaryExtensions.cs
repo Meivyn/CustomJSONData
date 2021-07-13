38c51f57685798dfcae119ec7c749d771d873b37
Aeroluna <aeroluna5@gmail.com>
2021-07-12 19:10:51 -0700

    FromJSON

--- CustomJSONData/Utils/DictionaryExtensions.cs
+++ CustomJSONData/Utils/DictionaryExtensions.cs
@@ -2,9 +2,26 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+    using Newtonsoft.Json;
 
     public static class DictionaryExtensions
     {
+        public static Dictionary<string, object?> FromJSON(string jsonString)
+        {
+            using MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(jsonString));
+            using JsonTextReader reader = new JsonTextReader(new StreamReader(jsonString));
+            return FromJSON(reader);
+        }
+
+        public static Dictionary<string, object?> FromJSON(JsonReader reader, Func<string, bool>? specialCase = null)
+        {
+            Dictionary<string, object?> dictioanry = new Dictionary<string, object?>();
+            reader.ReadToDictionary(dictioanry, specialCase);
+            return dictioanry;
+        }
+
         public static Dictionary<string, object?> Copy(this Dictionary<string, object?> dictionary)
         {
             return dictionary != null ? new Dictionary<string, object?>(dictionary) : new Dictionary<string, object?>();
