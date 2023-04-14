c0775beb1d713ef5e667048cb539434700655a4e
Aeroluna <aeroluna5@gmail.com>
2023-04-13 21:50:31 -0700

    use GetCustomType in CallbacksInTime

--- CustomJSONData/CustomBeatmap/CustomBeatmapData.cs
+++ CustomJSONData/CustomBeatmap/CustomBeatmapData.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using HarmonyLib;
 using IPA.Utilities;
 using JetBrains.Annotations;
---
+++ CustomJSONData/HarmonyPatches/CustomBeatmapData/CallbackCustomTypeGetter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using CustomJSONData.CustomBeatmap;
+using HarmonyLib;
+
+namespace CustomJSONData.HarmonyPatches
+{
+    [HarmonyPatch(typeof(CallbacksInTime))]
+    internal static class CallbackCustomTypeGetter
+    {
+        private static readonly MethodInfo _getType = AccessTools.Method(typeof(object), nameof(GetType));
+        private static readonly MethodInfo _getCustomType = AccessTools.Method(typeof(CustomBeatmapData), nameof(CustomBeatmapData.GetCustomType));
+
+        [HarmonyTranspiler]
+        [HarmonyPatch(nameof(CallbacksInTime.CallCallbacks), typeof(BeatmapDataItem))]
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            return new CodeMatcher(instructions)
+                .MatchForward(false, new CodeMatch(OpCodes.Callvirt, _getType))
+                .Set(OpCodes.Call, _getCustomType)
+                .InstructionEnumeration();
+        }
+    }
+}
